c29cfc3d6a46c67eefd29d9dbd2f933a68cb5e76
mangaorphy <156855506+mangaorphy@users.noreply.github.com>
2024-09-10 15:39:23 +0200

    Update settings.py
    added djoser library

--- LittleLemonFUllProject 2/littlelemon/settings.py
+++ LittleLemonFUllProject 2/littlelemon/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
     # The settings for app updated for the Graded assessment
     'restaurant',
 ]
